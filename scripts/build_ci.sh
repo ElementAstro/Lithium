cf876b480ae81227cdab92307054f19f35f671c1
AstroAir <astro_air@126.com>
2023-04-15 11:28:33 +0800

    fix ci

--- scripts/build_ci.sh
+++ scripts/build_ci.sh
@@ -1,7 +1,1 @@
-sudo apt-add-repository ppa:mutlaqja/ppa -y
-
-sudo apt update && sudo apt upgrade -y
-
-sudo apt install libspdlog-dev libboost-dev libgsl-dev libcfitsio-dev libz-dev python3-dev libssl-dev libsqlite3-dev
-
-sudo apt-get install libindi1 indi-bin libindi-dev
+sudo apt-add-repository ppa:mutlaqja/ppa -y && sudo apt update && sudo apt upgrade -y && sudo apt install libspdlog-dev libboost-dev libgsl-dev libcfitsio-dev libz-dev python3-dev libssl-dev libsqlite3-dev && sudo apt-get install libindi1 indi-bin libindi-dev
